ccf91f8bc100d3f8c954ce23df7df3a1ba5e861d
Simon Dodsley <simon@purestorage.com>
2022-06-13 14:35:14 -0400

    Update fusion_array.py

--- plugins/modules/fusion_array.py
+++ plugins/modules/fusion_array.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # (c) 2022, Simon Dodsley (simon@purestorage.com)
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING.GPLv3 or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
