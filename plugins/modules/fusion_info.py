7ddfaee9cb7148ef423b306c4d47cf82e7e41d50
Simon Dodsley <simon@purestorage.com>
2022-06-13 14:39:54 -0400

    Update fusion_info.py

--- plugins/modules/fusion_info.py
+++ plugins/modules/fusion_info.py
@@ -2,17 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # (c) 2021, Simon Dodsley (simon@purestorage.com)
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING.GPLv3 or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
-
-ANSIBLE_METADATA = {
-    "metadata_version": "1.1",
-    "status": ["preview"],
-    "supported_by": "community",
-}
 
 DOCUMENTATION = r"""
 ---
