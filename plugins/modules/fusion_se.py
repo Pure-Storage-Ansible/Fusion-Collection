3f57ef8700f7563d83156f935ff7d2df75aa1272
Jan Kodera <jkodera@purestorage.com>
2023-02-01 14:48:48 +0100

    Update copyright

--- plugins/modules/fusion_se.py
+++ plugins/modules/fusion_se.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2022, Simon Dodsley (simon@purestorage.com)
+# (c) 2023, Simon Dodsley (simon@purestorage.com), Jan Kodera (jkodera@purestorage.com)
 # GNU General Public License v3.0+ (see COPYING.GPLv3 or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
