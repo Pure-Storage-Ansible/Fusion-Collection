edf352dae399c7049840ad94ea6f8f5220a31846
Jan Kodera <jkodera@purestorage.com>
2023-02-08 14:31:47 +0100

    Fix networking unit tests boilerplate

--- tests/unit/module_utils/networking.py
+++ tests/unit/module_utils/networking.py
@@ -3,6 +3,10 @@
 # (c) 2023, Jan Kodera (jkodera@purestorage.com)
 # GNU General Public License v3.0+ (see COPYING.GPLv3 or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
 from ansible_collections.purestorage.fusion.plugins.module_utils.networking import (
     is_valid_address,
     is_valid_network,
