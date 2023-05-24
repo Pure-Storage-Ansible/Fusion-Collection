486db394ce0c01ed02dd878d9b711a292b343573
Jan Hrazdira <jhrazdira@purestorage.com>
2023-05-24 17:19:15 +0200

    Remove reading of Fusion env vars

--- plugins/module_utils/fusion.py
+++ plugins/module_utils/fusion.py
@@ -112,10 +112,6 @@ def get_fusion(module):
         module.fail_on_missing_params([PARAM_PRIVATE_KEY_FILE])
 
     config = fusion.Configuration()
-    if ENV_API_HOST in environ or ENV_HOST in environ:
-        host_url = environ.get(ENV_API_HOST, environ.get(ENV_HOST))
-        config.host = urljoin(host_url, BASE_PATH)
-    config.token_endpoint = environ.get(ENV_TOKEN_ENDPOINT, config.token_endpoint)
 
     if access_token is not None:
         config.access_token = access_token
@@ -124,13 +120,6 @@ def get_fusion(module):
         config.private_key_file = private_key_file
         if private_key_password is not None:
             config.private_key_password = private_key_password
-    elif ENV_ACCESS_TOKEN in environ:
-        config.access_token = environ.get(ENV_ACCESS_TOKEN)
-    elif (
-        ENV_ISSUER_ID in environ or ENV_APP_ID in environ
-    ) and ENV_PRIVATE_KEY_FILE in environ:
-        config.issuer_id = environ.get(ENV_ISSUER_ID, environ.get(ENV_APP_ID))
-        config.private_key_file = environ.get(ENV_PRIVATE_KEY_FILE)
     else:
         module.fail_json(
             msg=f"You must set either {ENV_ISSUER_ID} and {ENV_PRIVATE_KEY_FILE} or {ENV_ACCESS_TOKEN} environment variables. "
